3650600fcbb702bf4220ea4da786652850a5c5cb
NikolaiSemko <semk0001@algonquinlive.com>
2025-06-05 19:50:58 -0400

    Add architecture diagram and demo screenshot

--- main.tf
+++ main.tf
@@ -24,3 +24,161 @@ provider "azurerm" {
 provider "cloudinit" {
   # Configuration options
 }
+
+# Variables
+variable "labelPrefix" {
+  description = "Your college username. This will form the beginning of various resource names."
+  type        = string
+}
+
+variable "region" {
+  description = "Azure region for resources"
+  type        = string
+  default     = "Canada Central"
+}
+
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+  default     = "azureadmin"
+}
+
+# Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = "${var.labelPrefix}-A05-RG"
+  location = var.region
+}
+
+# Public IP Address
+resource "azurerm_public_ip" "main" {
+  name                = "${var.labelPrefix}-A05-PublicIP"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  sku                = "Standard"
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.labelPrefix}-A05-VNet"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Subnet
+resource "azurerm_subnet" "main" {
+  name                 = "${var.labelPrefix}-A05-Subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Network Security Group
+resource "azurerm_network_security_group" "main" {
+  name                = "${var.labelPrefix}-A05-NSG"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Network Interface Card
+resource "azurerm_network_interface" "main" {
+  name                = "${var.labelPrefix}-A05-NIC"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
+
+# Associate Network Security Group with Network Interface
+resource "azurerm_network_interface_security_group_association" "main" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
+# Cloud-init configuration
+data "cloudinit_config" "main" {
+  gzip          = true
+  base64_encode = true
+
+  part {
+    filename     = "init.sh"
+    content_type = "text/x-shellscript"
+    content      = file("${path.module}/init.sh")
+  }
+}
+
+# Virtual Machine
+resource "azurerm_linux_virtual_machine" "main" {
+  name                = "${var.labelPrefix}-A05-VM"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+
+  # Disable password authentication and use SSH keys
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "latest"
+  }
+
+  custom_data = data.cloudinit_config.main.rendered
+}
+
+# Output Values
+output "resource_group_name" {
+  description = "The name of the resource group"
+  value       = azurerm_resource_group.main.name
+}
+
+output "public_ip_address" {
+  description = "The public IP address of the web server"
+  value       = azurerm_public_ip.main.ip_address
+}
